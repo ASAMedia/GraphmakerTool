af795dc89f45b93fc59dbb0d22ff07fb5f554855
ASAMedia <schorchtaxel@gmail.com>
2019-05-09 23:21:26 +0200

    Update runOnWindows.ps1

--- runOnWindows.ps1
+++ runOnWindows.ps1
@@ -1,2 +1,3 @@
+javac --module-path "$((get-location).path)\lib\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls .\src\graphmakerwindow\*.java
 java -classpath "$((get-location).path)\src\"  --module-path "$((get-location).path)\lib\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls graphmakerwindow.GraphmakerWindow
 pause
